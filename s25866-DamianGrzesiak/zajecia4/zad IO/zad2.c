00c15801e926433d7e68017cd186d479f793fecd
s25866-pj <s25866@pjwstk.edu.pl>
2022-06-24 10:51:00 +0200

    add file from 4 chapter

--- s25866-DamianGrzesiak/zajecia4/zad IO/zad2.c
+++ s25866-DamianGrzesiak/zajecia4/zad IO/zad2.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char** argv){
-    int i;
-    int *tab=malloc(sizeof(int)*i);
+    int* tab;
+    int wersy;
+  int i;
     FILE * file;
     file = fopen("test.txt","r");
+    
     if(file==NULL){
         printf("nie ma takiego pliku\n");
         return -1;
 
     }else{
-        nfgetc(file);
-        for(i=0;i<10;i++){
-            printf("%c",num[i]);
-        }
-     
+        fscanf(file,"%d\r\n", &wersy); /* odczytywanie pełnego wersu*/
+        tab = (int*)malloc(wersy * sizeof(int));
+        /*printf("tyle wersow czytam %i :\n",wersy);*/
+        for (i=0;i<wersy;i++){
+           fscanf(file,"%i\r\n", &*tab+i);
+         }
+         for (i=wersy-2;i>=0;i--){
+           printf("%d\n",tab[i]);
+         }
+       free(tab);
         fclose(file);
         return 0;
 
